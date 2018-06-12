9666a72ce42afe15d5ef1c393a58807d856d5c16
jordan elley <jordan.elley@myob.com>
2018-06-13 10:19:33 +1200

    writting http request tests

---
+++ Greetings_Website.test/RequestOrganiserShould.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Greetings_Website.test
+{
+    public class RequestOrganiserShould
+    {
+        [Fact]
+        public void PrintGreetingOnPostRequest()
+        {
+            var requestOrganiser = new RequestOrganiser();
+            
+        }
+        
+    }
+
+    public class RequestOrganiser
+    {
+    }
+}
